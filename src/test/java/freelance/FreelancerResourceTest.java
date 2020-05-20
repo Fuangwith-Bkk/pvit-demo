c9d8542d60afeb193453f57fea7ff644f3e7c50e
voravitl <voravitl@gmail.com>
2020-05-20 19:40:37 +0700

    Remove DB test and readiness

--- src/test/java/freelance/FreelancerResourceTest.java
+++ src/test/java/freelance/FreelancerResourceTest.java
@@ -28,114 +28,114 @@ import static javax.ws.rs.core.Response.Status;
 @QuarkusTest
 public class FreelancerResourceTest {
   private static final Logger logger = Logger.getLogger(FreelancerResourceTest.class);
-  @Test
-  public void testFindAll() {
-    logger.info("Test GET all Ids");
-    given()
-      .when().get("/freelancers")
-      .then()
-      .statusCode(Status.OK.getStatusCode());
+  // @Test
+  // public void testFindAll() {
+  //   logger.info("Test GET all Ids");
+  //   given()
+  //     .when().get("/freelancers")
+  //     .then()
+  //     .statusCode(Status.OK.getStatusCode());
 
-  }
+  // }
 
-  @Test
-  public void testFindById() {
-    logger.info("Test GET id: 123456");
-    given()
-      .when().get("/freelancers/123456")
-      .then()
-      .statusCode(Status.OK.getStatusCode());
+  // @Test
+  // public void testFindById() {
+  //   logger.info("Test GET id: 123456");
+  //   given()
+  //     .when().get("/freelancers/123456")
+  //     .then()
+  //     .statusCode(Status.OK.getStatusCode());
 
-  }
+  // }
 
-  @Test
-  public void testDeleteById() {
-    logger.info("Test DELETE id: 123456");
-    given()
-      .when().delete("/freelancers/123456")
-      .then()
-      .statusCode(Status.NO_CONTENT.getStatusCode());
-    logger.info("Check that 123456 is deleted");
-    given()
-      .when().get("/freelancers/123456")
-      .then()
-      .statusCode(Status.NOT_FOUND.getStatusCode());
+  // @Test
+  // public void testDeleteById() {
+  //   logger.info("Test DELETE id: 123456");
+  //   given()
+  //     .when().delete("/freelancers/123456")
+  //     .then()
+  //     .statusCode(Status.NO_CONTENT.getStatusCode());
+  //   logger.info("Check that 123456 is deleted");
+  //   given()
+  //     .when().get("/freelancers/123456")
+  //     .then()
+  //     .statusCode(Status.NOT_FOUND.getStatusCode());
 
-  }
+  // }
 
-  @Test
-  public void createFreelancer() throws JsonProcessingException {
-    Freelancer freelancer = new Freelancer();
-    freelancer.setId(999999L);
-    freelancer.setFirstName("Joe");
-    freelancer.setLastName("Elliott");
-    freelancer.setEmail("jelliott@defleppard.com");
-    ObjectMapper objectMapper = new ObjectMapper();
-    String freelancerAsString = objectMapper.writeValueAsString(freelancer);
-    logger.info("Test POST with: "+freelancerAsString);
-    given()
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(freelancerAsString)
-      .post("/freelancers")
-      .then()
-      .statusCode(Status.CREATED.getStatusCode());
-    given()
-      .get("/freelancers/999999")
-      .then()
-      .statusCode(Status.OK.getStatusCode());
-  }
-  @Test
-  public void updateFreelancer() throws JsonProcessingException {
-    Freelancer freelancer = new Freelancer();
-    freelancer.setId(888888L);
-    freelancer.setFirstName("Joe");
-    freelancer.setLastName("Elliott");
-    freelancer.setEmail("jelliott@defleppard.com");
-    ObjectMapper objectMapper = new ObjectMapper();
-    String freelancerAsString = objectMapper.writeValueAsString(freelancer);
-    logger.info("Test POST with: "+freelancerAsString);
-    given()
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(freelancerAsString)
-      .post("/freelancers")
-      .then()
-      .statusCode(Status.CREATED.getStatusCode());
+  // @Test
+  // public void createFreelancer() throws JsonProcessingException {
+  //   Freelancer freelancer = new Freelancer();
+  //   freelancer.setId(999999L);
+  //   freelancer.setFirstName("Joe");
+  //   freelancer.setLastName("Elliott");
+  //   freelancer.setEmail("jelliott@defleppard.com");
+  //   ObjectMapper objectMapper = new ObjectMapper();
+  //   String freelancerAsString = objectMapper.writeValueAsString(freelancer);
+  //   logger.info("Test POST with: "+freelancerAsString);
+  //   given()
+  //     .contentType(MediaType.APPLICATION_JSON)
+  //     .body(freelancerAsString)
+  //     .post("/freelancers")
+  //     .then()
+  //     .statusCode(Status.CREATED.getStatusCode());
+  //   given()
+  //     .get("/freelancers/999999")
+  //     .then()
+  //     .statusCode(Status.OK.getStatusCode());
+  // }
+  // @Test
+  // public void updateFreelancer() throws JsonProcessingException {
+  //   Freelancer freelancer = new Freelancer();
+  //   freelancer.setId(888888L);
+  //   freelancer.setFirstName("Joe");
+  //   freelancer.setLastName("Elliott");
+  //   freelancer.setEmail("jelliott@defleppard.com");
+  //   ObjectMapper objectMapper = new ObjectMapper();
+  //   String freelancerAsString = objectMapper.writeValueAsString(freelancer);
+  //   logger.info("Test POST with: "+freelancerAsString);
+  //   given()
+  //     .contentType(MediaType.APPLICATION_JSON)
+  //     .body(freelancerAsString)
+  //     .post("/freelancers")
+  //     .then()
+  //     .statusCode(Status.CREATED.getStatusCode());
 
-    given()
-      .get("/freelancers/888888")
-      .then()
-      .assertThat()
-      .statusCode(Status.OK.getStatusCode())
-      .body("email", equalTo(freelancer.getEmail()))
-      .body("lastName", equalTo(freelancer.getLastName()))
-      .body("firstName",equalTo(freelancer.getFirstName()));
-  }
-  @Test
-  public void updateExistingFreelancer() throws JsonProcessingException{
-    Freelancer freelancer = new Freelancer();
-    String newEmail = "jteller@rider.com";
-    freelancer.setId(123456L);
-    freelancer.setEmail(newEmail);
-    ObjectMapper objectMapper = new ObjectMapper();
-    String freelancerAsString = objectMapper.writeValueAsString(freelancer);
+  //   given()
+  //     .get("/freelancers/888888")
+  //     .then()
+  //     .assertThat()
+  //     .statusCode(Status.OK.getStatusCode())
+  //     .body("email", equalTo(freelancer.getEmail()))
+  //     .body("lastName", equalTo(freelancer.getLastName()))
+  //     .body("firstName",equalTo(freelancer.getFirstName()));
+  // }
+  // @Test
+  // public void updateExistingFreelancer() throws JsonProcessingException{
+  //   Freelancer freelancer = new Freelancer();
+  //   String newEmail = "jteller@rider.com";
+  //   freelancer.setId(123456L);
+  //   freelancer.setEmail(newEmail);
+  //   ObjectMapper objectMapper = new ObjectMapper();
+  //   String freelancerAsString = objectMapper.writeValueAsString(freelancer);
     
-    logger.info("Test PUT for existing freelancer with: "+freelancerAsString);
-    given()
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(freelancerAsString)
-      .put("/freelancers/123456")
-      .then()
-      .statusCode(Status.NO_CONTENT.getStatusCode());
+  //   logger.info("Test PUT for existing freelancer with: "+freelancerAsString);
+  //   given()
+  //     .contentType(MediaType.APPLICATION_JSON)
+  //     .body(freelancerAsString)
+  //     .put("/freelancers/123456")
+  //     .then()
+  //     .statusCode(Status.NO_CONTENT.getStatusCode());
 
-     given()
-      .get("/freelancers/123456")
-      .then()
-      .assertThat()
-      .statusCode(Status.OK.getStatusCode())
-      .body("email", equalTo("jteller@rider.com")); 
+  //    given()
+  //     .get("/freelancers/123456")
+  //     .then()
+  //     .assertThat()
+  //     .statusCode(Status.OK.getStatusCode())
+  //     .body("email", equalTo("jteller@rider.com")); 
      
     
-  }
+  // }
   @Test
 void shouldPingLiveness() {
     given()
@@ -144,20 +144,20 @@ void shouldPingLiveness() {
         .statusCode(Status.OK.getStatusCode());
 }
 
-@Test
-void shouldPingReadiness() {
-    given()
-        .when().get("/health/ready")
-        .then()
-        .statusCode(Status.OK.getStatusCode());
-}
-@Test
-void shouldPingHealth() {
-  given()
-      .when().get("/health")
-      .then()
-      .statusCode(Status.OK.getStatusCode());
-}
+// @Test
+// void shouldPingReadiness() {
+//     given()
+//         .when().get("/health/ready")
+//         .then()
+//         .statusCode(Status.OK.getStatusCode());
+//}
+// @Test
+// void shouldPingHealth() {
+//   given()
+//       .when().get("/health")
+//       .then()
+//       .statusCode(Status.OK.getStatusCode());
+// }
 @Test
 void shouldPingMetrics() {
     given()
