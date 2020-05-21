2bd42ca9c1fe321a759119544dc00b739870d063
voravitl <voravitl@gmail.com>
2020-05-21 13:02:44 +0700

    Update SQL files

--- etc/sql/schema.sql
+++ etc/sql/schema.sql
@@ -1,16 +1,17 @@
-DROP TABLE IF EXISTS FREELANCER_SKILLS;
 DROP TABLE IF EXISTS FREELANCER;
+DROP TABLE IF EXISTS FREELANCER_SKILLS;
 CREATE TABLE FREELANCER (
-  id integer primary key,
+  id integer PRIMARY KEY,
   firstname varchar(50),
   lastname varchar(50),
   email varchar(50)
 );
 CREATE TABLE FREELANCER_SKILLS (
-  id integer not null,
+  id SERIAL PRIMARY KEY,
   skill varchar(50),
   detail varchar(100),
-  freelancer_id integer not null,
+  freelancer_id integer,
   FOREIGN KEY (freelancer_id) REFERENCES freelancer(id)
 );
+
 
